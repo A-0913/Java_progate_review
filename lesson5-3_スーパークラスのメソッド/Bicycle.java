c72040fa4b3e00b79ad534f5eb94e6655aaac179
EC2 Default User <ayaka-31014313@outlook.jp>
2023-01-08 07:38:13 +0000

    lesson5-3_スーパークラスのメソッド復習

---
+++ lesson5-3_スーパークラスのメソッド/Bicycle.java
@@ -0,0 +1,4 @@
+//3.Carクラスとの重複部分を切り取る。
+//5.BicycleクラスがVehicleクラスを継承するようにする。
+class Bicycle extends Vehicle  {
+}
---
+++ lesson5-3_スーパークラスのメソッド/Car.java
@@ -0,0 +1,4 @@
+//2.Bicycleクラスとの重複部分を切り取って、Vehicleクラスに移動する。
+//4.CarクラスがVehicleクラスを継承するようにする。
+class Car extends Vehicle {
+}
